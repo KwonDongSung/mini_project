a91379a04b48fae57baeb25c21d39f64a8a0081d
KwonDongSung <21700045@handong.edu>
2020-04-12 01:35:30 +0900

    Added load, saveproduct function name to manager.h

--- manager.h
+++ manager.h
@@ -4,6 +4,8 @@
 
 void listproduct(Product *p, int count);
 int selectdatano(Product *p, int count);
+void saveproduct(Product *p, int count);
+int loadproduct(Product *p);
 void searchname(Product *p, int count);
 void searchprice(Product *p, int count);
 void searchstar(Product *p, int count);
