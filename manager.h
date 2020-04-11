57e0a2097db848a6593253d8f020767a17475a6f
KwonDongSung <21700045@handong.edu>
2020-04-12 00:42:45 +0900

    Added searching function names to manager.h

--- manager.h
+++ manager.h
@@ -4,3 +4,7 @@
 
 void listproduct(Product *p, int count);
 int selectdatano(Product *p, int count);
+void searchname(Product *p, int count);
+void searchprice(Product *p, int count);
+void searchstar(Product *p, int count);
+
